f23cbee16098fc27178b13c511c3a8b92ef0ed16
Julian Büning <julian.buening@rwth-aachen.de>
2017-02-24 13:53:40 +0100

    MemoryTrace: split search in findLoop() into two phases

--- lib/Core/MemoryTrace.cpp
+++ lib/Core/MemoryTrace.cpp
@@ -11,6 +11,43 @@
 #include <iterator>
 #include <sstream>
 
+/*
+
+   Example: Internal Data Structures of MemoryTrace
+
+   1. registerBasicBlock(inst 1, fingerprint 1);
+   2. registerBasicBlock(inst 2, fingerprint 2);
+   3. registerBasicBlock(inst 3, fingerprint 3);
+   4. registerEndOfStackFrame(delta 1, true);
+   5. registerBasicBlock(inst 4, fingerprint 4);
+   6. registerBasicBlock(inst 5, fingerprint 5);
+   7. registerBasicBlock(inst 6, fingerprint 6);
+   8. registerEndOfStackFrame(delta 2, false);
+   9. registerBasicBlock(inst 7, fingerprint 7);
+
+
+   std::vector<MemoryTraceEntry>
+              stack
+
+       inst      fingerprint                      std::vector<StackFrameEntry>
+   +---------+----------------+                            stackFrames
+ 6 | inst 7  | fingerprint 7  |
+ \==<==============<===================<======+     index   delta    allocas
+ 5 | inst 6  | fingerprint 6  |   \            \  +-------+---------+-------+
+   +---------+----------------+    \            +-|---{ 6 | delta 2 | false | 1
+ 4 | inst 5  | fingerprint 5  |     +- Stack-     +-------+---------+-------+
+   +---------+----------------+    /   frame 1  +-|---{ 3 | delta 1 | true  | 0
+ 3 | inst 4  | fingerprint 4  |   /            /  +-------+---------+-------+
+ \==<==============<===================<======+         |
+ 2 | inst 3  | fingerprint 3  |   \                     +- index marks the first
+   +---------+----------------+    \                       entry that belongs
+ 1 | inst 2  | fingerprint 2  |     +- Stack-              to the next stack
+   +---------+----------------+    /   frame 0             frame
+ 0 | inst 1  | fingerprint 1  |   /
+   +---------+----------------+--+
+
+*/
+
 namespace klee {
 
 void MemoryTrace::registerBasicBlock(const KInstruction *instruction,
@@ -79,35 +116,50 @@ bool MemoryTrace::findLoop() {
   }
 
   auto stackFramesIt = stackFrames.rbegin();
-  size_t currentStackFrameBoundary = 0;
+  std::size_t topStackFrameBoundary = 0;
   if(stackFramesIt != stackFrames.rend()) {
     // first index that belongs to current stack frame
-    currentStackFrameBoundary = stackFramesIt->index;
+    topStackFrameBoundary = stackFramesIt->index;
   }
 
-  MemoryTraceEntry &topEntry = stack.back();
-  auto it = stack.rbegin();
-  ++it; // skip first element
-  for (; it != stack.rend(); ++it) {
-    if (topEntry == *it) {
-      // found an entry with same PC and fingerprint
-      return true;
-    }
+  // calculate number of entries within first stack frame
+  std::size_t topStackFrameEntries = stack.size() - topStackFrameBoundary;
 
-    size_t index = std::distance(stack.begin(), it.base()) - 1;
-    if(stackFramesIt != stackFrames.rend() &&
-       currentStackFrameBoundary == index) {
-      // entering new stack frame in next iteration
-      if(stackFramesIt->allocas) {
-        // stack frame contained allocas, thus we cannot find any further match
-        klee_warning_once(stack[currentStackFrameBoundary-1].inst,
+  // Phase 1:
+  // find matching entries within first stack frame
+  if (topStackFrameEntries > 1) {
+    MemoryTraceEntry &topEntry = stack.back();
+    auto it = stack.rbegin() + 1; // skip first element
+    for (; it != stack.rbegin() + topStackFrameEntries; ++it) {
+      // iterate over all elements within the first stack frame (but the first)
+      if (topEntry == *it) {
+        // found an entry with same PC and fingerprint
+        return true;
+      }
+    }
+  }
+
+  // Phase 2:
+  // for all following stack frames it suffices to find a match of the first
+  // entry within a stack frame
+  if(stackFrames.size() > 0) {
+    MemoryTraceEntry &topStackFrameBase = stack.at(topStackFrameBoundary);
+
+    for (auto it = stackFrames.rbegin() + 1; it != stackFrames.rend(); ++it) {
+      // iterate over all stack frames (but the first)
+
+      if(it->allocas) {
+        // stack frame containes allocas, thus we cannot find any further match
+        klee_warning_once(stack[topStackFrameBoundary-1].inst,
           "previous stack frame contains alloca, "
           "aborting search for infinite loops at this location");
         return false;
-      } else {
-        // find boundary of previous stack frame
-        ++stackFramesIt;
-        currentStackFrameBoundary = stackFramesIt->index;
+      }
+
+      MemoryTraceEntry &stackFrame = stack.at(it->index);
+      if (topStackFrameBase == stackFrame) {
+        // PC and iterator are the same at stack frame base
+        return true;
       }
     }
   }
