f031047db5fd55702e2f38a9484058b29ba8ac09
Julian Büning <julian.buening@rwth-aachen.de>
2017-05-26 15:11:57 +0200

    Liveness testsuite: improved circular-list.c

--- test/Liveness/circular-list.c
+++ test/Liveness/circular-list.c
@@ -3,14 +3,30 @@
 // RUN: %llvmgcc %s -emit-llvm -O2 -g -c -o %t-O2.bc
 // RUN: %llvmgcc %s -emit-llvm -O3 -g -c -o %t-O3.bc
 
+// TODO: check if difference between line 65 and 68 is significat or due to debug info
+
 // RUN: rm -rf %t-O0.klee-out
-// RUN: %klee -output-dir=%t-O0.klee-out -detect-infinite-loops %t-O0.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O0.klee-out -detect-infinite-loops -allocate-determ %t-O0.bc > %t-O0.log 2>&1
+// RUN: test -f %t-O0.klee-out/test000001.infty.err
+// RUN: cat %t-O0.log | FileCheck %s
+
 // RUN: rm -rf %t-O1.klee-out
-// RUN: %klee -output-dir=%t-O1.klee-out -detect-infinite-loops %t-O1.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O1.klee-out -detect-infinite-loops -allocate-determ %t-O1.bc > %t-O1.log 2>&1
+// RUN: test -f %t-O0.klee-out/test000001.infty.err
+// RUN: cat %t-O1.log | FileCheck %s
+
 // RUN: rm -rf %t-O2.klee-out
-// RUN: %klee -output-dir=%t-O2.klee-out -detect-infinite-loops %t-O2.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O2.klee-out -detect-infinite-loops -allocate-determ %t-O2.bc > %t-O2.log 2>&1
+// RUN: test -f %t-O0.klee-out/test000001.infty.err
+// RUN: cat %t-O2.log | FileCheck %s
+
 // RUN: rm -rf %t-O3.klee-out
-// RUN: %klee -output-dir=%t-O3.klee-out -detect-infinite-loops %t-O3.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O3.klee-out -detect-infinite-loops -allocate-determ %t-O3.bc > %t-O3.log 2>&1
+// RUN: test -f %t-O0.klee-out/test000001.infty.err
+// RUN: cat %t-O3.log | FileCheck %s
+
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <klee/klee.h>
 
@@ -19,7 +35,7 @@ struct node {
   int entry;
 };
 
-int main(void) {
+int main(int argc, char *argv[]) {
   unsigned x;
   klee_make_symbolic(&x, sizeof(x), "x");
   klee_assume(x < 10); // 10 paths, 9 of which are infinite
@@ -45,9 +61,14 @@ int main(void) {
   if (prev) // at least one node exists, create circular list
     prev->next = head;
 
-  // CHECK: infinite loop
-  while (head)
+  // CHECK: KLEE: ERROR: {{[^:]*}}/circular-list.c:{{[0-9]+}}: infinite loop{{$}}
+  while (head) {
+    printf("LIST ENTRY [%d]: %d\n", x, head->entry);
     head = head->next;
+  }
 
+  // CHECK: KLEE: done: completed paths = 10
+  // CHECK: KLEE: done: generated tests = 1
+  klee_silent_exit(0);
   return 0;
 }
