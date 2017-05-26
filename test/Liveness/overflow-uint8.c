905e0c4ac1cd642aea49dbd1a6b9b952789d9819
Julian Büning <julian.buening@rwth-aachen.de>
2017-05-26 15:39:30 +0200

    Liveness testsuite: run whiletrue.c and overflow-uint8.c with uclibc, posix-runtime and allocate-determ

--- test/Liveness/overflow-uint8.c
+++ test/Liveness/overflow-uint8.c
@@ -4,16 +4,19 @@
 // RUN: %llvmgcc %s -emit-llvm -O3 -g -c -o %t-O3.bc
 
 // RUN: rm -rf %t-O0.klee-out
-// RUN: %klee -output-dir=%t-O0.klee-out -detect-infinite-loops %t-O0.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O0.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O0.bc 2>&1 | FileCheck %s
 // RUN: test -f %t-O0.klee-out/test000001.infty.err
+
 // RUN: rm -rf %t-O1.klee-out
-// RUN: %klee -output-dir=%t-O1.klee-out -detect-infinite-loops %t-O1.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O1.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O1.bc 2>&1 | FileCheck %s
 // RUN: test -f %t-O1.klee-out/test000001.infty.err
+
 // RUN: rm -rf %t-O2.klee-out
-// RUN: %klee -output-dir=%t-O2.klee-out -detect-infinite-loops %t-O2.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O2.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O2.bc 2>&1 | FileCheck %s
 // RUN: test -f %t-O2.klee-out/test000001.infty.err
+
 // RUN: rm -rf %t-O3.klee-out
-// RUN: %klee -output-dir=%t-O3.klee-out -detect-infinite-loops %t-O3.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O3.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O3.bc 2>&1 | FileCheck %s
 // RUN: test -f %t-O3.klee-out/test000001.infty.err
 
 #include <inttypes.h>
@@ -21,12 +24,12 @@
 
 #include <klee/klee.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
   int x;
   klee_make_symbolic(&x, sizeof(x), "x");
   klee_assume(x < 0);
 
-  // CHECK: infinite loop
+  // CHECK: KLEE: ERROR: {{[^:]*}}/overflow-uint8.c:{{[0-9]+}}: infinite loop{{$}}
   for (uint8_t i = 0; i != x; i++) {
     printf("%d\n", i);
   }
