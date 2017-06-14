853a86ffa148c50bf63290232959cdc62bd25e67
Julian Büning <julian.buening@rwth-aachen.de>
2017-06-14 10:54:53 +0200

    Liveness testsuite: getopt.c, regression test for previous bug fix

---
+++ test/Liveness/getopt.c
@@ -0,0 +1,62 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -g -c -o %t-O0.bc
+// RUN: %llvmgcc %s -emit-llvm -O1 -g -c -o %t-O1.bc
+// RUN: %llvmgcc %s -emit-llvm -O2 -g -c -o %t-O2.bc
+// RUN: %llvmgcc %s -emit-llvm -O3 -g -c -o %t-O3.bc
+
+// RUN: rm -rf %t-O0.klee-out
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O0.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O0.bc -a -a > %t-O0.log 2>&1
+// RUN: not test -f %t-O0.klee-out/test000001.infty.err
+// RUN: cat %t-O0.log | FileCheck %s
+
+// RUN: rm -rf %t-O1.klee-out
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O1.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O1.bc -a -a > %t-O1.log 2>&1
+// RUN: not test -f %t-O1.klee-out/test000001.infty.err
+// RUN: cat %t-O1.log | FileCheck %s
+
+// RUN: rm -rf %t-O2.klee-out
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O2.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O2.bc -a -a > %t-O2.log 2>&1
+// RUN: not test -f %t-O2.klee-out/test000001.infty.err
+// RUN: cat %t-O2.log | FileCheck %s
+
+// RUN: rm -rf %t-O3.klee-out
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O3.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O3.bc -a -a > %t-O3.log 2>&1
+// RUN: not test -f %t-O3.klee-out/test000001.infty.err
+// RUN: cat %t-O3.log | FileCheck %s
+
+// regression test: this triggered an infinite loop with -a -a when registering
+// basic blocks on return from a function (in this case getopt_long)
+
+
+#include <getopt.h>
+#include <stdio.h>
+
+static struct option const longopts[] = {
+  {"long", no_argument, NULL, 'a'},
+  {"longer", no_argument, NULL, 'b'},
+  {NULL, 0, NULL, 0}
+};
+
+int main(int argc, char *argv[]) {
+  while (1) {
+    // CHECK-NOT: KLEE: ERROR: {{[^:]*}}/getopt.c:{{[0-9]+}}: infinite loop{{$}}
+    int c = getopt_long(argc, argv, "+abc", longopts, NULL);
+    printf("%d\n", c);
+    if (c == -1) {
+      break;
+    }
+
+    switch (c) {
+      case 'a':
+        printf("a\n");
+        break;
+
+      case 'b':
+        printf("b\n");
+        break;
+
+      case 'c':
+        printf("c\n");
+        break;
+    }
+  }
+}
