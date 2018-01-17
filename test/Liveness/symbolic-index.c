18edb36b5ab956e90cc12689790b7e43fc14af2b
Julian Büning <julian.buening@rwth-aachen.de>
2018-01-17 16:47:03 +0100

    Liveness testsuite: improve symbolic-index.c

--- test/Liveness/symbolic-index.c
+++ test/Liveness/symbolic-index.c
@@ -3,14 +3,23 @@
 // RUN: %llvmgcc %s -emit-llvm -O2 -g -c -o %t-O2.bc
 // RUN: %llvmgcc %s -emit-llvm -O3 -g -c -o %t-O3.bc
 
+// -libc=uclibc -posix-runtime not needed, only added to test under more "realistic" circumstances
+
 // RUN: rm -rf %t-O0.klee-out
-// RUN: %klee -output-dir=%t-O0.klee-out -detect-infinite-loops -stop-after-n-instructions=1000 %t-O0.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O0.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O0.bc 2>&1 | FileCheck %s
+// RUN: test -f %t-O0.klee-out/test000001.infty.err
+
 // RUN: rm -rf %t-O1.klee-out
-// RUN: %klee -output-dir=%t-O1.klee-out -detect-infinite-loops -stop-after-n-instructions=1000 %t-O1.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O1.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O1.bc 2>&1 | FileCheck %s
+// RUN: test -f %t-O1.klee-out/test000001.infty.err
+
 // RUN: rm -rf %t-O2.klee-out
-// RUN: %klee -output-dir=%t-O2.klee-out -detect-infinite-loops -stop-after-n-instructions=1000 %t-O2.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O2.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O2.bc 2>&1 | FileCheck %s
+// RUN: test -f %t-O2.klee-out/test000001.infty.err
+
 // RUN: rm -rf %t-O3.klee-out
-// RUN: %klee -output-dir=%t-O3.klee-out -detect-infinite-loops -stop-after-n-instructions=1000 %t-O3.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O3.klee-out -detect-infinite-loops -stop-after-n-instructions=10000 -allocate-determ %t-O3.bc 2>&1 | FileCheck %s
+// RUN: test -f %t-O3.klee-out/test000001.infty.err
 
 #include <inttypes.h>
 #include <klee/klee.h>
@@ -24,10 +33,13 @@ int main(int argc, char **argv) {
   array[1] = 1;
   array[2] = 2;
   if(x >= 0 && x <= 2) {
-    // CHECK: infinite loop
+    // CHECK: KLEE: ERROR: {{[^:]*}}/symbolic-index.c:{{[0-9]+}}: infinite loop{{$}}
     while(array[x] == 1) {
       array[x + 1] = 0;
       array[x - 1] = 1;
     }
   }
+
+  // do not output any test cases for terminating runs
+  klee_silent_exit(0);
 }
