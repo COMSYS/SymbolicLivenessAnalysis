4304ee271de52e66210dff194acf28ceb1c2e0a7
Julian Büning <julian.buening@rwth-aachen.de>
2018-09-04 13:03:54 +0200

    Liveness testsuite: increase instruction budget
    to be able to successfully run these tests with
    KLEE_RUNTIME_BUILD_TYPE=Debug

--- test/Liveness/control-flow.c
+++ test/Liveness/control-flow.c
@@ -12,7 +12,7 @@
 // RUN: test -f %t-O0.klee-out/test000005.infty.err
 
 // RUN: rm -rf %t-O1.klee-out
-// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O1.klee-out -detect-infinite-loops -emit-all-errors -stop-after-n-instructions=10000 -allocate-determ %t-O1.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O1.klee-out -detect-infinite-loops -emit-all-errors -stop-after-n-instructions=100000 -allocate-determ %t-O1.bc 2>&1 | FileCheck %s
 // RUN: test -f %t-O1.klee-out/test000001.infty.err
 // RUN: test -f %t-O1.klee-out/test000002.infty.err
 // RUN: test -f %t-O1.klee-out/test000003.infty.err
@@ -20,7 +20,7 @@
 // RUN: test -f %t-O1.klee-out/test000005.infty.err
 
 // RUN: rm -rf %t-O2.klee-out
-// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O2.klee-out -detect-infinite-loops -emit-all-errors -stop-after-n-instructions=10000 -allocate-determ %t-O2.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O2.klee-out -detect-infinite-loops -emit-all-errors -stop-after-n-instructions=100000 -allocate-determ %t-O2.bc 2>&1 | FileCheck %s
 // RUN: test -f %t-O2.klee-out/test000001.infty.err
 // RUN: test -f %t-O2.klee-out/test000002.infty.err
 // RUN: test -f %t-O2.klee-out/test000003.infty.err
@@ -28,7 +28,7 @@
 // RUN: test -f %t-O2.klee-out/test000005.infty.err
 
 // RUN: rm -rf %t-O3.klee-out
-// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O3.klee-out -detect-infinite-loops -emit-all-errors -stop-after-n-instructions=10000 -allocate-determ %t-O3.bc 2>&1 | FileCheck %s
+// RUN: %klee -libc=uclibc -posix-runtime -output-dir=%t-O3.klee-out -detect-infinite-loops -emit-all-errors -stop-after-n-instructions=100000 -allocate-determ %t-O3.bc 2>&1 | FileCheck %s
 // RUN: test -f %t-O3.klee-out/test000001.infty.err
 // RUN: test -f %t-O3.klee-out/test000002.infty.err
 // RUN: test -f %t-O3.klee-out/test000003.infty.err
